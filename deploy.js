a095f225bc38779384aada8f0a50b8d92e6738fe
tobyvazu2-dev <ibrahim.bejaoui155255@hotmail.com>
2024-12-30 09:50:37 +0100

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,36 +1,45 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config');
-const bot = new Client({ intents: [GatewayIntentBits.Guilds] });
+const { REST, Routes } = require('discord.js');
+const { token, clientId } = require('./config');
 
-bot.once('ready', async () => {
-  const commands = [
-    new SlashCommandBuilder().setName('info').setDescription('See the info of the bot'),
-    new SlashCommandBuilder().setName('kick').setDescription('Kicks a member from the server')
-      .addUserOption(option =>
-        option.setName('member')
-          .setDescription('The member to kick.')
-          .setRequired(true))
-      .addStringOption(option =>
-        option.setName('reason')
-          .setDescription('The reason for the kick.'))
-  ]
-    .map(command => command.toJSON());
+const commands = [
+    {
+        name: 'info',
+        description: 'See the info of the bot',
+    },
+    {
+        name: 'kick',
+        description: 'Kicks a member from the server',
+        options: [
+            {
+                type: 6, // USER
+                name: 'member',
+                description: 'The member to kick',
+                required: true,
+            },
+            {
+                type: 3, // STRING
+                name: 'reason',
+                description: 'The reason for the kick',
+                required: false,
+            },
+        ],
+    },
+];
 
-  try {
-    const existingCommands = await bot.application.commands.fetch();
-    for (const command of commands) {
-      const existingCommand = existingCommands.find(cmd => cmd.name === command.name);
-      if (existingCommand) {
-        await bot.application.commands.edit(existingCommand.id, command);
-      } else {
-        await bot.application.commands.create(command);
-      }
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+    try {
+        console.log('Refreshing application commands...');
+
+        await rest.put(
+            Routes.applicationCommands(clientId),
+            { body: commands },
+        );
+
+        console.log('Successfully registered application commands.');
+    } catch (error) {
+        console.error('Error while registering commands:', error);
     }
-    console.log('Commands successfully registered.');
-  } catch (error) {
-    console.error('Error registering commands:', error);
-  }
-});
+})();
 
-bot.login(token);
